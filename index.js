93001bb72ac27f9192a774a2c9bda939ff1643da
James Arnold <j.arnold@buzzinteractive.co.uk>
2024-06-11 18:07:02 +0100

    integrate james AI chatbot

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ document.documentElement.classList.add('js-enabled');
 import { setupAnimations } from './component/animations.js';
 import setupCarousel from './component/carousel.js';
 import { setUpModal } from './component/modal.js';
+import { chatbotTheme } from './theme/theme.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     setUpModal();
     setupCarousel();
     setupAnimations();
-});
+    import("https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js").then((module) => {
+        const Chatbot = module.default;
+        Chatbot.init({
+            chatflowid: "e761d2f6-af46-43a7-baf7-f5e54c1bd0fe",
+            apiHost: "https://flowise-ibs6.onrender.com",
+            theme: chatbotTheme
+        });
+})
+})
---
+++ theme/theme.js
@@ -0,0 +1,58 @@
+const BRAND_COLOURS = {
+    MAIN: "#00aaa8",
+    SECONDARY: "#413c58",
+    ERROR: '#303235'
+}
+
+export const chatbotTheme = {
+    button: {
+      backgroundColor: BRAND_COLOURS.SECONDARY,
+      right: 20,
+      bottom: 20,
+      size: 86, // small | medium | large | number
+      dragAndDrop: true,
+      iconColor: "white",
+    //   customIconSrc: "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/google-messages.svg",
+    },
+    chatWindow: {
+      showTitle: true,
+      title: 'James AI',
+      titleAvatarSrc: 'https://github.com/james-arn/portfolio/blob/main/images/headshot-crop-light-blue.png?raw=true',
+      welcomeMessage: 'Hello!',
+      errorMessage: "Ooops, there's been an error",
+      backgroundColor: "#ffffff",
+    //   height: 700,
+    //   width: 400,
+      fontSize: 16,
+      poweredByTextColor: "#303235",
+      botMessage: {
+        backgroundColor: "#f7f8ff",
+        textColor: "#303235",
+        showAvatar: true,
+        avatarSrc: "https://github.com/james-arn/portfolio/blob/main/images/headshot-crop-light-blue.png?raw=true",
+      },
+      userMessage: {
+        backgroundColor: BRAND_COLOURS.SECONDARY,
+        textColor: "#ffffff",
+        showAvatar: true,
+        avatarSrc: "https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/usericon.png",
+      },
+      textInput: {
+        placeholder: 'Ask me a question',
+        backgroundColor: '#ffffff',
+        textColor: '#303235',
+        sendButtonColor: BRAND_COLOURS.SECONDARY,
+        maxChars: 200,
+        maxCharsWarningMessage: 'You exceeded the characters limit. Please input less than 200 characters.',
+      },
+      feedback: {
+        color: BRAND_COLOURS.ERROR,
+      },
+      footer: {
+        textColor: '#303235',
+        text: '',
+        company: '',
+        companyLink: '',
+      }
+    }
+  }
